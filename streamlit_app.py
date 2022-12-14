061a6daa30154d1f1e26785194c9e7e9a75516cf
Aaron Padam <120459896+AaronPadam@users.noreply.github.com>
2022-12-14 18:26:53 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,7 +35,7 @@ def get_fruityvice_data(this_fruit_choice):
 # new section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
 try:
-fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
     streamlit.error("Please select a fruit to get information")
   else:
@@ -50,7 +50,7 @@ streamlit.header("View our Fruit List - Add Your Favourites!")
 # snowflake functions
 def get_fruit_load_list():
   with my_cnx.cursor() as my_cur:
-    my_cur.execute("SELECT * FROM fruit_load_list")
+    my_cur.execute("SELECT * FROM fruit_load_list")
     return my_cur.fetchall()
 # adding a button to load the fruit
 if streamlit.button('Get Fruit List'):
